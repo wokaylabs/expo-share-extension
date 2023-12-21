ba50d7cd8873ab110133e2335d2c733596f4fe76
Rahul Ranjan <rr842001@gmail.com>
2023-12-21 14:00:13 +0530

    fix: native module not found in ios

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,3 @@ import ExpoShareExtensionModule from "./ExpoShareExtension";
 export const close = ExpoShareExtensionModule.close;
 
 export const getIntentData = ExpoShareExtensionModule.getIntentData;
-
-export const ACTIVITY_NAME =  ExpoShareExtensionModule.ACTIVITY_NAME;
